356de6c8ef3df2a440f8ad6c18ccb63496d8256d
Igor Mandrigin <i@mandrigin.ru>
2022-07-05 09:35:01 +0200

    debugs

--- rpc/server.go
+++ rpc/server.go
@@ -18,6 +18,7 @@ package rpc
 
 import (
 	"context"
+	"fmt"
 	"io"
 	"sync/atomic"
 
@@ -117,6 +118,7 @@ func (s *Server) serveSingleRequest(ctx context.Context, codec ServerCodec) {
 		}
 		return
 	}
+	fmt.Println("IGORM: RPC server received batch", reqs)
 	if batch {
 		h.handleBatch(reqs)
 	} else {
@@ -128,6 +130,7 @@ func (s *Server) serveSingleRequest(ctx context.Context, codec ServerCodec) {
 // requests to finish, then closes all codecs which will cancel pending requests and
 // subscriptions.
 func (s *Server) Stop() {
+	fmt.Println("IGORM: RPC server stopping")
 	if atomic.CompareAndSwapInt32(&s.run, 1, 0) {
 		log.Info("RPC server shutting down")
 		s.codecs.Each(func(c interface{}) bool {
